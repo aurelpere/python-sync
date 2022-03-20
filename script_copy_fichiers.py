ea8af956b24087bdce484fbbde6c9969310a6c5e
aurelpere <aurel.pere@gmail.com>
2022-03-20 18:41:01 +0100

    adding ci/cd and refreshing code

--- script_copy_fichiers.py
+++ script_copy_fichiers.py
@@ -1,287 +1,242 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
-# script qui recopie un répertoire dans un autre répertoire
-# définir source et dest avant de lancer le script
-# version brutecopy sans identifier les modifs
-di = "/"
-df = "/home/user/df"
-kw = "brave"
+"""
+this is script_copy_fichiers.py
+"""
 
-import shutil
 import os
-import time
 import filecmp
+import re
+import argparse
+import shutil
+import py7zr
 
-
-def mkdirs(newdir, mode=777):
+def mkdirs(newdir):
+    "make dir with 777 mode"
     try:
-        os.makedirs(newdir, mode)
+        os.makedirs(newdir)
+        return 1
     except OSError as err:
         return err
 
-def test_mkdirs():
-    mkdirs('testfolder')
-    assert os.path.isdir('testfolder')==True
-    
-def contain(chaine1, chaine2):
-    "true si chaine1 inclut ds chaine2"
-    chaine1 = chaine1.lower()
-    chaine2 = chaine2.lower()
-    L = len(chaine1)
-    if len(chaine2) < len(chaine1):
-        return False
-    for i in range(len(chaine2) - L + 1):
-        slicee = ""
-        for j in range(L):
-            slicee += chaine2[i + j]
-        if slicee == chaine1: 
-            return True
-    return False
-
-def test_contain():
-    assert contain('test','this is a test')==True
-    assert contain('test','this is a sentence')==False
-
-def listFiles(folder):
-    "la fonction listFiles renvoie une liste des fichiers du répertoire courant contenant kw"
+def listfileskw(folder, keyword=""):
+    "la fonction listfiles renvoie une liste des fichiers du répertoire folder contenant kw"
+    currentfolder=os.getcwd()
     os.chdir(folder)
     files = []
-    a = os.listdir()
-    b = []
-    for i in a:
-        if os.path.isfile(i):
-            files.append(i)
+    dirlist = os.listdir()
+    templist = []
+    for i in dirlist:
+        if keyword!="":
+            compiledkw=re.compile(r'{}'.format(keyword))
+            if os.path.isfile(i) and re.search(compiledkw, i):
+                files.append(i)
+            else:
+                templist.append(i)
         else:
-            b.append(i)
+            if os.path.isfile(i):
+                files.append(i)
+            else:
+                templist.append(i)
+    os.chdir(currentfolder)
     return files
 
-def test_listFiles():
-    assert sorted(listFiles('/Users/macbook/github/scraping'))==['boxplot_scraping.png','README.md','scraping.py']
-
-
-
-def listFileskw(folder, kw=""):
-    "la fonction listFiles renvoie une liste des fichiers du répertoire folder contenant kw"
-    os.chdir(folder)
-    files = []
-    a = os.listdir()
-    b = []
-    for i in a:
-        if os.path.isfile(i) and contain(i, kw):
-            files.append(i)
-        else:
-            b.append(i)
-    return files
-
-def test_listFileskw():
-    assert sorted(listFileskw('/Users/macbook/github/scraping',kw='scraping'))==['boxplot_scraping.png','scraping.py']
-
-
-
-def listFolders(folder):
-    "la fonction listFolders renvoie une liste des noms des sousrépertoires du repertoire folder"
+def listfolders(folder):
+    "la fonction listfolders renvoie une liste des noms des sousrépertoires du repertoire folder"
+    currentfolder=os.getcwd()
     os.chdir(folder)
     folders = []
-    a = os.listdir()
-    b = []
-    for i in a:
+    dirlist = os.listdir()
+    templist = []
+    for i in dirlist:
         if os.path.isdir(i):
             folders.append(i)
         else:
-            b.append(i)
+            templist.append(i)
+    os.chdir(currentfolder)
     return folders
 
-def test_listFolders():
-    assert sorted(listFolders('/Users/macbook/github/scraping'))==['subfolder']
-
-
-
-def dicoisation(dico, folder):
+def dico_maker(folder):
     "renvoie un dictionnaire des fichiers et repertoires de folder"
-    a = listFiles(folder)
-    b = listFolders(folder)
-    dico[folder] = a
-    for m in b:
-        dico[folder + "/" + m] = listFiles(folder + "/" + m)
-        dicoisation(dico, folder + "/" + m)
+    dico={}
+    fileslist = listfileskw(folder)
+    folderlist = listfolders(folder)
+    dico[folder] = fileslist
+    for fold in folderlist:
+        dico[f'{folder}/{fold}'] = listfileskw(f'{folder}/{fold}')
+        dico_maker(f'{folder}/{fold}')
     return dico
 
-def test_dicoisation():
-    assert (dicoisation('/Users/macbook/github/scraping'))=={'/Users/macbook/github/scraping':['boxplot_scraping.png','README.md','scraping.py'],'/Users/macbook/github/scraping/subfolder':['']}
-
-
-
-def recopier(source, dest):
-    "recopie les fichiers du repertoire source ainsi que ses sous repertoire vers le repertoire dest et copie le plus recent s'ils ont le meme nom"
-
-    a = listFiles(source)
-    b = listFolders(source)
-    timestampsrc = 0
-    timestampdest = 0
+def recopier(source, dest,keyword=''):
+    """recopie les fichiers du repertoire source ainsi que ses
+    sous repertoire vers le repertoire dest et copie le plus recent s'ils ont le meme nom"""
+    if keyword!='':
+        fileslist = listfileskw(source,keyword)
+    else:
+        fileslist=listfileskw(source)
+    folderlist = listfolders(source)
     identique = []
-    # print(a)
-    for i in a:
-        if os.path.isfile(str(dest) + "/" + i):  # si le fichier dest existe
-            if not (
-                filecmp.cmp(str(i), str(dest) + "/" + str(i))
-            ):  # si le fichier dest est différent du fichier source
-                timestampdest = os.stat(str(dest) + "/" + i)
-                timestampsrc = os.stat(str(source) + "/" + i)
-                if (
-                    timestampsrc[8] > timestampdest[8]
-                ):  # si le fichier source est plus récent que le fichier dest
-                    # stat[8] donne le mtime cad le temps en seconde depuis lequel le fichier a été modifié. copyfile conserve cette propriété
-                    goodcopy(dest, str(i))
-                    # print(str(i))
+    for i in fileslist:
+        if os.path.isfile(f'{dest}/{i}'):  # si le fichier dest existe
+            if not filecmp.cmp(f'{source}/{i}', f'{dest}/{i}'):
+            # si le fichier dest est différent du fichier source
+                timestampdest = os.stat(f'{dest}/{i}')
+                timestampsrc = os.stat(f'{source}/{i}')
+                if timestampsrc[8] > timestampdest[8]:
+                    # si le fichier source est plus récent que le fichier dest
+                    # stat[8] donne le mtime cad le temps en seconde depuis lequel le
+                    # fichier a été modifié. copyfile conserve cette propriété
+                    try:
+                        shutil.copy2(f'{source}/{i}', f'{dest}/{i}',follow_symlinks=False)
+                    except Exception as err:
+                        print (err)
+                        pythoncopy(f'{source}/{i}', f'{dest}/{i}')
+            else:
+            # sinon on ajoute les fichiers identiques à une liste qui n'est pas utilise
+                identique.append(i)
+        else:  # si le fichier dest n'existe pas
+            try:
+                shutil.copy2(f'{source}/{i}', f'{dest}/{i}', follow_symlinks=False)
+            except Exception as err:
+                print(err)
+                pythoncopy(f'{source}/{i}', f'{dest}/{i}')
+    for j in folderlist:
+        if os.path.isdir(f'{dest}/{j}'):
+            print(f'{dest}/{j} existe')
+        else:
+            mkdirs(f'{dest}/{j}')
+        recopier(f'{source}/{j}', f'{dest}/{j}')
+
+def pythoncopy(src_file,dest_file):
+    "copy src to dest_folder bytes by bytes. used to replace shutil.copy2"
+    with open(src_file,'rb') as filesrc:
+        filelist=filesrc.readlines()
+    with open(f'{dest_file}','wb') as filedest:
+        for i in filelist:
+            filedest.write(i)
+
+def chiffrer(source, dest,keyword='',password=''):
+    """la fonction chiffrer(source,dest) 7zippe avec chiffrement les fichiers
+    du repertoire source ainsi que ses sous repertoire vers le repertoire dest
+    s'ils n'existent pas ou s'ils sont plus recent (pr le meme nom)"""
+    cwd=os.getcwd()
+    if keyword!='':
+        fileslist = listfileskw(source,keyword)
+    else:
+        fileslist=listfileskw(source)
+    folderlist = listfolders(source)
+    identique = []
+    for i in fileslist:
+        if os.path.isfile(f'{dest}/{i}.7z'):  # si le fichier dest existe en .7z
+            timestampdest = os.stat(f'{dest}/{i}.7z')
+            timestampsrc = os.stat(f'{source}/{i}')
+            if timestampsrc[8] > timestampdest[8]:
+                # si le timestamp source est plus récent que celui du dest
+                with py7zr.SevenZipFile(f'{dest}/{i}.7z', mode='w',password=password) as archive:
+                    os.chdir(source)
+                    archive.write(f'./{i}')
+                    os.chdir(cwd)
             else:  # sinon on ajoute les fichiers identiques à une liste qui n'est pas utilise
                 identique.append(i)
         else:  # si le fichier dest n'existe pas
-            goodcopy(dest, str(i))
-            # print(str(i))
-    for j in b:
-        if os.path.isdir(str(dest) + "/" + j):
-            print(str(dest) + "/" + j + "existe ")
+            with py7zr.SevenZipFile(f'{dest}/{i}.7z', mode='w',password=password) as archive:
+                os.chdir(source)
+                archive.write(f'./{i}')
+                os.chdir(cwd)
+
+    for j in folderlist:
+        if os.path.isdir(f'{dest}/{j}'):
+            print(f"{dest}/{j} existe ou il y a eu un message d'erreur de copie")
         else:
-            mkdirs(str(dest) + "/" + j)
-        recopier(str(source) + "/" + j, str(dest) + "/" + j)
+            mkdirs(f'{dest}/{j}')
+        chiffrer(f'{source}/{j}', f'{dest}/{j}')
 
-def test_recopier():
-    recopier('/Users/macbook/github/scraping','/Users/macbook')
-    assert os.path.isfile('/Users/macbook/boxplot_scraping.png')==True
-    assert os.path.isfile('/Users/macbook/README.md')==True
-    assert os.path.isfile('/Users/macbook/scraping.py')==True
-
-    
-def chiffrer(source, dest):
-    "la fonction chiffrer(source,dest) 7zippe avec chiffrement les fichiers du repertoire source ainsi que ses sous repertoire vers le repertoire dest s'ils n'existent pas ou s'ils sont plus recent (pr le meme nom)"
-    a = listFiles(source)
-    b = listFolders(source)
-    timestampsrc = 0
-    timestampdest = 0
+def dechiffrer(source, dest,keyword='',password=''):
+    """la fonction dechiffrer(source,dest) utilise 7zipp pour extraire les
+     archives du repertoire source et ses sous repertoires vers le repertoire
+     dest s'ils n'existent pas ou s'ils sont plus recent (pr le meme nom)"""
+    if keyword!='':
+        fileslist = listfileskw(source,r'({}).+(\.7z$)'.format(keyword))
+    else:
+        fileslist=listfileskw(source,'.7z')
+    folderlist = listfolders(source)
     identique = []
-    # print(a)
-    for i in a:
-        if os.path.isfile(
-            str(dest) + "/" + i + ".7z"
-        ):  # si le fichier dest existe en .7z
-            timestampdest = os.stat(str(dest) + "/" + i + ".7z")
-            timestampsrc = os.stat(str(source) + "/" + i)
-            if (
-                timestampsrc[8] > timestampdest[8]
-            ):  # si le timestamp source est plus récent que celui du dest
-                prgmrun = (
-                    "7z a -mhe=on -pmdp "
-                    + '"'
-                    + str(dest)
-                    + "/"
-                    + str(i)
-                    + ".7z"
-                    + '"'
-                    + ' "'
-                    + str(i)
-                    + '"'
-                    + " -scrc SHA256 -stl"
-                )
-                os.system(prgmrun)  # ajouter un overwrite
+    for i in fileslist:
+        no7z_i = f'{i}'.replace(".7z", " ").strip()
+        if os.path.isfile(f'{dest}/{no7z_i}'):  # si le fichier dest existe sans .7z
+            timestampdest = os.stat(f'{dest}/{no7z_i}')
+            timestampsrc = os.stat(f'{source}/{i}')
+            if timestampsrc[8] > timestampdest[8]:
+                with py7zr.SevenZipFile(f'{source}/{i}', mode='r', password=password) as archive:
+                    archive.extractall(path=dest)
             else:  # sinon on ajoute les fichiers identiques à une liste qui n'est pas utilise
                 identique.append(i)
         else:  # si le fichier dest n'existe pas
-            prgmrun = (
-                "7z a -mhe=on -pmdp "
-                + '"'
-                + str(dest)
-                + "/"
-                + str(i)
-                + ".7z"
-                + '"'
-                + ' "'
-                + str(i)
-                + '"'
-                + " -scrc SHA256 -stl"
-            )
-            os.system(prgmrun)  # ajouter un overwrite
-    for j in b:
-        if os.path.isdir(str(dest) + "/" + j):
-            print(
-                str(dest)
-                + "/"
-                + j
-                + " existe ou il y a eu un message d'erreur de copie"
-            )
+            with py7zr.SevenZipFile(f'{source}/{i}', mode='r', password=password) as archive:
+                archive.extractall(path=dest)
+    for j in folderlist:
+        if os.path.isdir(f'{dest}/{j}'):
+            print(f"{dest}/{j} existe ou il y a eu un message d'erreur de copie")
         else:
-            mkdirs(str(dest) + "/" + j)
+            os.mkdir(f'{dest}/{j}')
+        dechiffrer(f'{source}/{j}', f'{dest}/{j}')
 
-        chiffrer(str(source) + "/" + j, str(dest) + "/" + j)
-
-def test_chiffrer():
-    chiffrer('/Users/macbook/github/scraping','/Users/macbook')
-    assert os.path.isfile('/Users/macbook/boxplot_scraping.png.7z')==True
-    assert os.path.isfile('/Users/macbook/README.md.7z')==True
-    assert os.path.isfile('/Users/macbook/scraping.py.7z')==True
-
-        
-def dechiffrer(source, dest):
-    "la fonction dechiffrer(source,dest) utilise 7zipp pour extraire les archives du repertoire source et ses sous repertoires vers le repertoire dest s'ils n'existent pas ou s'ils sont plus recent (pr le meme nom)"
-    a = listFiles(source)
-    b = listFolders(source)
-    timestampsrc = 0
-    timestampdest = 0
-    identique = []
-    # print(a)
-    for i in a:
-        x = str(i)
-        y = x.replace(".7z", " ")
-        z = y.strip()  # stripping de i
-        if os.path.isfile(str(dest) + "/" + z):  # si le fichier dest existe sans .7z
-            timestampdest = os.stat(str(dest) + "/" + z)
-            timestampsrc = os.stat(str(source) + "/" + i)
-            if (
-                timestampsrc[8] > timestampdest[8]
-            ):  # si le timestamp source est plus récent que celui du dest
-                prgmrun = (
-                    "7z e "
-                    + str(source)
-                    + "/"
-                    + '"'
-                    + i
-                    + '"'
-                    + " -pmdp -o"
-                    + str(dest)
-                )
-                os.system(prgmrun)
-            else:  # sinon on ajoute les fichiers identiques à une liste qui n'est pas utilise
-                identique.append(i)
-        else:  # si le fichier dest n'existe pas
-            prgmrun = (
-                "7z e " + str(source) + "/" + '"' + i + '"' + " -pmdp -o" + str(dest)
-            )
-            os.system(prgmrun)
-    for j in b:
-        if os.path.isdir(str(dest) + "/" + j):
-            print(
-                str(dest)
-                + "/"
-                + j
-                + " existe ou il y a eu un message d'erreur de copie"
-            )
-        else:
-            os.mkdir(str(dest) + "/" + j)
-
-        dechiffrer(str(source) + "/" + j, str(dest) + "/" + j)
-
-def test_dechiffrer():
-    chiffrer('/Users/macbook/github/scraping','/Users/macbook')
-    dechiffrer('/Users/macbook','/Users/macbook/testdechiffrer')
-    assert os.path.isfile('/Users/macbook/testdechiffrer/boxplot_scraping.png')==True
-    assert os.path.isfile('/Users/macbook/testdechiffrer/README.md')==True
-    assert os.path.isfile('/Users/macbook/testdechiffrer/scraping.py')==True
-        
-
+def script_copy_fichiers(sourcefolder,destfolder,keyword='',cypher='',decypher=''):
+    """copy files from sourcefolder to destfolder and only copy files containing
+    keyword if defined. cypher dest files if cypher=1"""
+    sourcedict=dico_maker(sourcefolder)
+    keyslist = list(sourcedict.keys())
+    if '~/' in destfolder:
+        tildexpand = os.path.expanduser('~/')
+        destfolder=destfolder.replace('~/',tildexpand)
+    if not os.path.isdir(destfolder):
+        mkdirs(destfolder)
+    if cypher=='' and decypher=='':
+        for key in keyslist:  # key is a folder
+            recopier(key, destfolder,keyword=keyword)
+    elif cypher!='' and decypher=='':
+        for key in keyslist:
+            chiffrer(key,destfolder,password=cypher,keyword=keyword)
+    elif cypher!='' and decypher!='':
+        print('chooose either to cypher or decypher')
+    else:
+        dechiffrer(sourcefolder,destfolder,password=decypher,keyword=keyword)
 if __name__ == "__main__":
-    a = dicoisation({}, "/home/user/Téléchargements")
-    keys = list(a.keys())
-    for i in keys:  # i est un repertoire
-        if contain("brave", i):
-            print(i)
-            recopier(i, "/home/user/df")
+    parser = argparse.ArgumentParser(description='python script_copy_fichiers.py csvfile year')
+    parser.add_argument('-o',
+                        '--origin',
+                        required=True,
+                        metavar='origin folder',
+                        type=str,
+                        help='origin folder to process')
+    parser.add_argument('-d',
+                        '--dest',
+                        required=True,
+                        metavar='destination folder',
+                        type=str,
+                        help='destination folder to copy to')
+    parser.add_argument('-k',
+                        '--keyword',
+                        default='',
+                        metavar='keyword: will copy files only if keyword included in filenames',
+                        type=str,
+                        help='keyword that must be included in filesnames to process copy')
+    parser.add_argument('-c',
+                        '--cypher',
+                        default='',
+                        metavar='will cypher with 7zip with the password provided',
+                        type=str,
+                        help='will cypher the copy with 7ziping files with the password provided')
+    parser.add_argument('-dc',
+                        '--decypher',
+                        default='',
+                        metavar='will decypher with 7zip with the password provided',
+                        type=str,
+                        help='will decypher the copied files with the password provided')
 
+    args = vars(parser.parse_args())
+    script_copy_fichiers(args['origin'],
+                         args['dest'],
+                         keyword=args['keyword'],
+                         cypher=args['cypher'],
+                         decypher=args['decypher'])
